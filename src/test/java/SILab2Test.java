22c88ce3b73a1baf7f5636a91ac347eb27704802
darjanz <4034137-darjanz@users.noreply.gitlab.com>
2020-06-13 13:19:47 +0200

    SI-L2 first commit!

---
+++ src/test/java/SILab2Test.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SILab2Test {
+
+    @Test
+    void multipleCondition() {
+
+    }
+
+    @Test
+    void everyBranch() {
+
+    }
+}
